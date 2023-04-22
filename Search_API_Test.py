d47e01de455035b8b6c33995cc223f110b28d717
Alex Gershman <alex@gershman.org>
2023-04-22 14:49:42 -0700

    Search API - Returns Name, Price, and Link

--- Search_API_Test.py
+++ Search_API_Test.py
@@ -8,11 +8,11 @@ clothing_items = ['Broke', 'red hat', 'sweatervest', 'cowboy boots']
 
 # check if user is broke
 broke = False
-print('\n' + clothing_items[0])
 if(clothing_items[0] == 'Broke'):
     broke = True
-print(clothing_items)
 
+# function uses ScaleSerp API to search for clothing
+  # given name of an item to search for 10 possibilities
 def search_item(isBroke,item_num):
   params = {
       'api_key': '93194A5623F843C6ADE1B6DEE9125451',
@@ -20,6 +20,7 @@ def search_item(isBroke,item_num):
       'page': '1',
       'num': '10',
       'max_page': '1',
+      'location': 'New York City',
       'q': clothing_items[item_num]
     }
   if(isBroke):
@@ -29,12 +30,21 @@ def search_item(isBroke,item_num):
       'page': '1',
       'num': '10',
       'max_page': '1',
+      'location': 'New York City',
       'q': clothing_items[item_num] + ' cheap'
     }
   return requests.get('https://api.scaleserp.com/search', params)
+
+# Get name, price, and link of each item
 item = 0
-print('\n' + str(len(clothing_items)))
 while (item < (len(clothing_items)-1)):
   item = item + 1
-  item_data = search_item(broke,item).json()
-  print(json.dumps(item_data))
+  item_data =  search_item(broke,item).json()
+  for i in range(0,9,1):
+    item_name =  ((item_data["shopping_results"])[i])["title"]
+    item_price = ((item_data["shopping_results"])[i])["price_raw"]
+    item_link =  ((item_data["shopping_results"])[i])["link"]
+    print('\nName: ' + str(item_name))
+    print('Price: ' + str(item_price))
+    print('Link: '   + str(item_link))
+  
