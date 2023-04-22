76a6c21634a8c960503637a3a441b5858e57b3b9
Alex Gershman <alex@gershman.org>
2023-04-22 13:30:59 -0700

    Update Search_API_Test.py

--- Search_API_Test.py
+++ Search_API_Test.py
@@ -1,7 +1,23 @@
+import requests
+import json
+
+# output should be outfits listed in 5 strings
+  # isBool, hat/cover, top_item1, top_item2, bottom (?=none), shoes
+#clothing_items = input('ChatGPT Response: ').split(', ')
+clothing_items = ['Broke', 'red hat', 'sweatervest', 'cowboy boots']
+
+# check if user is broke
+broke = False
+print('\n' + clothing_items[0])
+if(clothing_items[0] == 'Broke'):
+    broke = True
+print(clothing_items)
+
 def search_item(isBroke,item_num):
   params = {
       'api_key': '93194A5623F843C6ADE1B6DEE9125451',
       'search_type': 'shopping',
+      'page': '1',
       'num': '10',
       'max_page': '1',
       'q': clothing_items[item_num]
